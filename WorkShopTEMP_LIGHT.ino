58cbf35b35649c9fcf390cee0f404a392c8c6731
fewgod <30302776+fewgod@users.noreply.github.com>
2017-07-22 23:17:01 -0700

    Add files via upload
    ลองเช็คดู แอร์ยังไม่ได้coกับ จำนวนคน แสงกับอุณหภูมิน่าจะโอเคแล้ว

---
+++ WorkShopTEMP_LIGHT.ino
@@ -0,0 +1,42 @@
+#define LED 2
+#define LIGHT A0
+//----------------
+#include <dht.h>
+#define DHT11 5
+//----------------
+
+//----------------
+int lightInput;
+//----------------
+dht DHT;
+int dhtTemp;
+int temperature;
+//----------------
+
+void setup() {
+  Serial.begin(9600);
+  //-----------------------
+  pinMode(LIGHT, INPUT);
+  pinMode(LED, OUTPUT);
+  //-----------------------
+  
+}
+
+void loop() {
+  //LIGHT--------------------------
+  lightInput = analogRead(LIGHT);
+  if(lightInput < 500){
+    digitalWrite(LED, LOW);
+  }
+  else{
+    digitalWrite(LED, HIGH);
+  }
+    Serial.print("Light Intensity: ");
+    Serial.println(lightInput);
+  //TEMPERATURE--------------------
+  dhtTemp = DHT.read11(DHT11);
+   temperature = (int) DHT.temperature;
+  Serial.println(temperature);
+  Serial.print("degree\n");
+  //-------------------------------
+}
---
+++ Workshop_Air.ino
@@ -0,0 +1,25 @@
+#define LED 3
+
+bool isPeople = true;
+bool isAirOn;
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() 
+{
+  if(isPeople == 1)
+  {
+    digitalWrite(LED, HIGH);
+    isAirOn = true;
+    Serial.print("Air Conditioner: ON");
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+    isAirOn = false;
+    Serial.print("Air Conditioner: OFF");
+  }
+  delay(200);
+}
